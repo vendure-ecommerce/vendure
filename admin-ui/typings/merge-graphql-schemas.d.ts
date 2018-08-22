9804be7e40a79e9e17caa6a5e1daf4a6d9601bf8
Michael Bromley <michael@michaelbromley.co.uk>
2018-08-22 12:59:26 +0200

    chore(admin-ui): Remove unused typings

--- admin-ui/typings/merge-graphql-schemas.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'merge-graphql-schemas' {
-    import * as MergeGraphqlSchemas from 'merge-graphql-schemas';
-
-    export function fileLoader(fileGlob: string): any;
-    export function mergeTypes(types: any[], options?: any): any;
-}
