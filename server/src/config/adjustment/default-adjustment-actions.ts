65f4a68423723375b4549f25af856db787a2dbf3
Michael Bromley <michael@michaelbromley.co.uk>
2018-10-09 11:32:14 +0200

    fix(server): Fix calculations for default adjustment actions

--- server/src/config/adjustment/default-adjustment-actions.ts
+++ server/src/config/adjustment/default-adjustment-actions.ts
@@ -7,7 +7,7 @@ export const orderPercentageDiscount: AdjustmentActionDefinition = {
     code: 'order_percentage_discount',
     args: [{ name: 'discount', type: 'percentage' }],
     calculate(order, args) {
-        return [{ amount: (order.totalPrice * args.discount) / 100 }];
+        return [{ amount: -(order.totalPrice * args.discount) / 100 }];
     },
     description: 'Discount order by { discount }%',
 };
@@ -19,7 +19,7 @@ export const itemPercentageDiscount: AdjustmentActionDefinition = {
     calculate(order, args) {
         return order.items.map(item => ({
             orderItemId: item.id,
-            amount: (item.totalPrice * args.discount) / 100,
+            amount: -(item.totalPrice * args.discount) / 100,
         }));
     },
     description: 'Discount every item by { discount }%',
