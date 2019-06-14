973acad04526628a41b977fe3d44d285dc681f7b
Michael Bromley <michael@michaelbromley.co.uk>
2019-06-14 09:59:56 +0200

    chore(dev-server): Add support for Elasticsearch plugin

--- packages/dev-server/dev-config.ts
+++ packages/dev-server/dev-config.ts
@@ -3,6 +3,7 @@ import { AdminUiPlugin } from '@vendure/admin-ui-plugin';
 import { AssetServerPlugin } from '@vendure/asset-server-plugin';
 import { ADMIN_API_PATH, API_PORT, SHOP_API_PATH } from '@vendure/common/lib/shared-constants';
 import { DefaultLogger, DefaultSearchPlugin, examplePaymentHandler, LogLevel, VendureConfig } from '@vendure/core';
+import { ElasticsearchPlugin } from '@vendure/elasticsearch-plugin';
 import { defaultEmailHandlers, EmailPlugin } from '@vendure/email-plugin';
 import path from 'path';
 import { ConnectionOptions } from 'typeorm';
@@ -36,9 +37,14 @@ export const devConfig: VendureConfig = {
         new AssetServerPlugin({
             route: 'assets',
             assetUploadDir: path.join(__dirname, 'assets'),
+            // assetUploadDir: path.join(__dirname, '../../../../kbart/artsupplies-vendure/server/vendure/assets'),
             port: 5002,
         }),
         new DefaultSearchPlugin(),
+        // new ElasticsearchPlugin({
+        //     host: 'http://192.168.99.100',
+        //     port: 9200,
+        // }),
         new EmailPlugin({
             devMode: true,
             handlers: defaultEmailHandlers,
