e660d4629a1dd57596046b7842f56a456beeb1ed
Michael Bromley <michael@michaelbromley.co.uk>
2019-07-30 16:03:17 +0200

    fix(core): Remove non-existent import

--- packages/core/src/api/index.ts
+++ packages/core/src/api/index.ts
@@ -1,6 +1,4 @@
 export * from './common/request-context';
 export * from './decorators/allow.decorator';
-export * from './decorators/decode.decorator';
 export * from './decorators/request-context.decorator';
 export * from './resolvers/admin/search.resolver';
-
