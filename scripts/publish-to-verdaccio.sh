79339b136bade32e5272504a052d76beffcf534e
Michael Bromley <michael@michaelbromley.co.uk>
2019-08-26 13:42:36 +0200

    chore: Create script for local publishing to Verdaccio

---
+++ scripts/publish-to-verdaccio.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# A shell script which publishes all packages to a local Verdaccio registry for testing / local dev purposes
+VERDACCIO=http://localhost:4873/
+
+cd ../packages/admin-ui-plugin && npm publish -reg $VERDACCIO &&\
+cd ../asset-server-plugin && npm publish -reg $VERDACCIO &&\
+cd ../common && npm publish -reg $VERDACCIO &&\
+cd ../core && npm publish -reg $VERDACCIO &&\
+cd ../create && npm publish -reg $VERDACCIO &&\
+cd ../elasticsearch-plugin && npm publish -reg $VERDACCIO &&\
+cd ../email-plugin && npm publish -reg $VERDACCIO
