a3afc1b9182cb0d410cee23437b4741e843c60ce
Michael Bromley <michael@michaelbromley.co.uk>
2019-12-06 13:38:55 +0100

    fix(core): Fix error when patching custom fields

--- packages/core/src/service/helpers/utils/patch-entity.ts
+++ packages/core/src/service/helpers/utils/patch-entity.ts
@@ -10,7 +10,7 @@ export type InputPatch<T> = { [K in keyof T]?: T[K] | null };
 export function patchEntity<T extends VendureEntity, I extends InputPatch<T>>(entity: T, input: I): T {
     for (const key of Object.keys(entity)) {
         const value = input[key as keyof T];
-        if (key === 'customFields') {
+        if (key === 'customFields' && value) {
             patchEntity((entity as any)[key], value as any);
         } else if (value !== undefined && key !== 'id') {
             entity[key as keyof T] = value as any;
