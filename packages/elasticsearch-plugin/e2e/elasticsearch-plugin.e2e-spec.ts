35d0008e4070fd4d25dcbac1c40f75105bd7b12a
Michael Bromley <michael@michaelbromley.co.uk>
2020-01-28 20:47:55 +0100

    fix(elasticsearch-plugin): Fix inconsistencies in behaviour between DBs

--- packages/elasticsearch-plugin/e2e/elasticsearch-plugin.e2e-spec.ts
+++ packages/elasticsearch-plugin/e2e/elasticsearch-plugin.e2e-spec.ts
@@ -64,7 +64,6 @@ describe('Elasticsearch plugin', () => {
 
     beforeAll(async () => {
         await server.init({
-            dataDir: path.join(__dirname, '__data__'),
             initialData,
             productsCsvPath: path.join(__dirname, 'fixtures/e2e-products-full.csv'),
             customerCount: 1,
