c74f0c8d2aa3f09a05d1ae04ea88c408238a977b
Michael Bromley <michael@michaelbromley.co.uk>
2020-01-29 15:31:42 +0100

    test(elasticsearch-plugin): Use http protocol in CI

--- packages/elasticsearch-plugin/e2e/elasticsearch-plugin.e2e-spec.ts
+++ packages/elasticsearch-plugin/e2e/elasticsearch-plugin.e2e-spec.ts
@@ -54,7 +54,7 @@ describe('Elasticsearch plugin', () => {
                 ElasticsearchPlugin.init({
                     indexPrefix: 'e2e-tests',
                     port: process.env.CI ? +(process.env.E2E_ELASTIC_PORT || 9200) : 9200,
-                    host: process.env.CI ? '127.0.0.1' : 'http://192.168.99.100',
+                    host: process.env.CI ? 'http://127.0.0.1' : 'http://192.168.99.100',
                 }),
             ],
         }),
