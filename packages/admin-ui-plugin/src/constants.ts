46ff9321831af856cbdab5092b75080d21d6bf7d
Michael Bromley <michael@michaelbromley.co.uk>
2020-03-24 10:25:26 +0100

    fix(admin-ui-plugin): Add Chinese to the default available languages

--- packages/admin-ui-plugin/src/constants.ts
+++ packages/admin-ui-plugin/src/constants.ts
@@ -1,4 +1,7 @@
+import { LanguageCode } from '@vendure/core';
 import path from 'path';
 
 export const DEFAULT_APP_PATH = path.join(__dirname, '../admin-ui');
 export const loggerCtx = 'AdminUiPlugin';
+export const defaultLanguage = LanguageCode.en;
+export const defaultAvailableLanguages = [LanguageCode.en, LanguageCode.es, LanguageCode.zh];
