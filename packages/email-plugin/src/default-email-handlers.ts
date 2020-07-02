4d598cb24ae1cab1f7a6ac14cfb9906915a6d506
Michael Bromley <michael@michaelbromley.co.uk>
2020-07-02 14:48:15 +0200

    refactor(email-plugin): Use new User structure in default handlers

--- packages/email-plugin/src/default-email-handlers.ts
+++ packages/email-plugin/src/default-email-handlers.ts
@@ -17,35 +17,35 @@ import {
 
 export const orderConfirmationHandler = new EmailEventListener('order-confirmation')
     .on(OrderStateTransitionEvent)
-    .filter(event => event.toState === 'PaymentSettled' && !!event.order.customer)
-    .setRecipient(event => event.order.customer!.emailAddress)
+    .filter((event) => event.toState === 'PaymentSettled' && !!event.order.customer)
+    .setRecipient((event) => event.order.customer!.emailAddress)
     .setFrom(`{{ fromAddress }}`)
     .setSubject(`Order confirmation for #{{ order.code }}`)
-    .setTemplateVars(event => ({ order: event.order }))
+    .setTemplateVars((event) => ({ order: event.order }))
     .setMockEvent(mockOrderStateTransitionEvent);
 
 export const emailVerificationHandler = new EmailEventListener('email-verification')
     .on(AccountRegistrationEvent)
-    .setRecipient(event => event.user.identifier)
+    .setRecipient((event) => event.user.identifier)
     .setFrom(`{{ fromAddress }}`)
     .setSubject(`Please verify your email address`)
-    .setTemplateVars(event => ({ user: event.user }))
+    .setTemplateVars((event) => ({ user: event.user }))
     .setMockEvent(mockAccountRegistrationEvent);
 
 export const passwordResetHandler = new EmailEventListener('password-reset')
     .on(PasswordResetEvent)
-    .setRecipient(event => event.user.identifier)
+    .setRecipient((event) => event.user.identifier)
     .setFrom(`{{ fromAddress }}`)
     .setSubject(`Forgotten password reset`)
-    .setTemplateVars(event => ({ user: event.user }))
+    .setTemplateVars((event) => ({ user: event.user }))
     .setMockEvent(mockPasswordResetEvent);
 
 export const emailAddressChangeHandler = new EmailEventListener('email-address-change')
     .on(IdentifierChangeRequestEvent)
-    .setRecipient(event => event.user.pendingIdentifier!)
+    .setRecipient((event) => event.user.getNativeAuthenticationMethod().pendingIdentifier!)
     .setFrom(`{{ fromAddress }}`)
     .setSubject(`Please verify your change of email address`)
-    .setTemplateVars(event => ({ user: event.user }))
+    .setTemplateVars((event) => ({ user: event.user }))
     .setMockEvent(mockEmailAddressChangeEvent);
 
 export const defaultEmailHandlers: Array<EmailEventHandler<any, any>> = [
