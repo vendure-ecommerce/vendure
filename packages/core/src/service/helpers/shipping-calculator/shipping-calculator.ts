c6c5ff67166a9e2f38d9f30ffe5fa5adaaa6c0f7
Jonathan Célio <jonathan.clio@hotmail.com>
2020-07-07 14:47:20 -0300

    feat(core): Await all promises at same time in ShippingCalculator

--- packages/core/src/service/helpers/shipping-calculator/shipping-calculator.ts
+++ packages/core/src/service/helpers/shipping-calculator/shipping-calculator.ts
@@ -6,6 +6,15 @@ import { Order } from '../../../entity/order/order.entity';
 import { ShippingMethod } from '../../../entity/shipping-method/shipping-method.entity';
 import { ShippingMethodService } from '../../services/shipping-method.service';
 
+type CheckEligibilityByShippingMethodResponse =
+    | {
+          method: ShippingMethod;
+          result: ShippingCalculationResult;
+      }
+    | undefined;
+
+type EligibleShippingMethod = NonNullable<CheckEligibilityByShippingMethodResponse>;
+
 @Injectable()
 export class ShippingCalculator {
     constructor(private shippingMethodService: ShippingMethodService) {}
@@ -14,21 +23,32 @@ export class ShippingCalculator {
      * Returns an array of each eligible ShippingMethod for the given Order and sorts them by
      * price, with the cheapest first.
      */
-    async getEligibleShippingMethods(
-        ctx: RequestContext,
-        order: Order,
-    ): Promise<Array<{ method: ShippingMethod; result: ShippingCalculationResult }>> {
+    async getEligibleShippingMethods(ctx: RequestContext, order: Order): Promise<EligibleShippingMethod[]> {
         const shippingMethods = this.shippingMethodService.getActiveShippingMethods(ctx.channel);
-        const eligibleMethods: Array<{ method: ShippingMethod; result: ShippingCalculationResult }> = [];
-        for (const method of shippingMethods) {
-            const eligible = await method.test(order);
-            if (eligible) {
-                const result = await method.apply(order);
-                if (result) {
-                    eligibleMethods.push({ method, result });
-                }
+
+        const checkEligibilityPromises = shippingMethods.map((method) =>
+            this.checkEligibilityByShippingMethod(order, method),
+        );
+        const eligibleMethods = await Promise.all(checkEligibilityPromises);
+
+        // Workaround to remove undefined from type: https://stackoverflow.com/a/51577579
+        const filteredEligibleMethods = eligibleMethods.filter(
+            (m): m is EligibleShippingMethod => m !== undefined,
+        );
+
+        return filteredEligibleMethods.sort((a, b) => a.result.price - b.result.price);
+    }
+
+    private async checkEligibilityByShippingMethod(
+        order: Order,
+        method: ShippingMethod,
+    ): Promise<CheckEligibilityByShippingMethodResponse> {
+        const eligible = await method.test(order);
+        if (eligible) {
+            const result = await method.apply(order);
+            if (result) {
+                return { method, result };
             }
         }
-        return eligibleMethods.sort((a, b) => a.result.price - b.result.price);
     }
 }
