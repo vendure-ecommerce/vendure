a1e03fd098c8f51eaf7f72dcebc4d52a623af502
Michael Bromley <michael@michaelbromley.co.uk>
2020-07-08 08:55:23 +0200

    fix(core): Fix error when de-serializing a RequestContext without expiry

--- packages/core/src/api/common/request-context.ts
+++ packages/core/src/api/common/request-context.ts
@@ -63,7 +63,10 @@ export class RequestContext {
         return new RequestContext({
             apiType: ctxObject._apiType,
             channel: new Channel(ctxObject._channel),
-            session: { ...ctxObject._session, expires: new Date(ctxObject._session.expires) },
+            session: {
+                ...ctxObject._session,
+                expires: ctxObject._session?.expires && new Date(ctxObject._session.expires),
+            },
             languageCode: ctxObject._languageCode,
             isAuthorized: ctxObject._isAuthorized,
             authorizedAsOwnerOnly: ctxObject._authorizedAsOwnerOnly,
