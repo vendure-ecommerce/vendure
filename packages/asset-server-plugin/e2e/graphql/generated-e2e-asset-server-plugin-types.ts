9fab7e89ef0a902272694cc17e1b9ab811f7f488
Thomas Blommaert <66438062+thomas-advantitge@users.noreply.github.com>
2020-08-12 16:41:02 +0200

    feat(core): Custom field length configuration for localeString

--- packages/asset-server-plugin/e2e/graphql/generated-e2e-asset-server-plugin-types.ts
+++ packages/asset-server-plugin/e2e/graphql/generated-e2e-asset-server-plugin-types.ts
@@ -1088,7 +1088,7 @@ export type DateRange = {
 };
 
 /**
- * Expects the same validation formats as the <input type="datetime-local"> HTML element.
+ * Expects the same validation formats as the `<input type="datetime-local">` HTML element.
  * See https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/datetime-local#Additional_attributes
  */
 export type DateTimeCustomFieldConfig = CustomField & {
@@ -1738,6 +1738,7 @@ export type LocaleStringCustomFieldConfig = CustomField & {
     __typename?: 'LocaleStringCustomFieldConfig';
     name: Scalars['String'];
     type: Scalars['String'];
+    length?: Maybe<Scalars['Int']>;
     label?: Maybe<Array<LocalizedString>>;
     description?: Maybe<Array<LocalizedString>>;
     readonly?: Maybe<Scalars['Boolean']>;
