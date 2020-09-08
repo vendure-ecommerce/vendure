0a4d460fbabbea267af24682f87918b9b2e57c9d
Michael Bromley <michael@michaelbromley.co.uk>
2020-09-08 16:42:43 +0200

    feat(admin-ui): Customer address editor opens in modal

--- packages/admin-ui/src/lib/customer/src/components/address-card/address-card.component.ts
+++ packages/admin-ui/src/lib/customer/src/components/address-card/address-card.component.ts
@@ -1,6 +1,16 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    ChangeDetectorRef,
+    Component,
+    EventEmitter,
+    Input,
+    OnInit,
+    Output,
+} from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { CustomFieldConfig, GetAvailableCountries } from '@vendure/admin-ui/core';
+import { CustomFieldConfig, GetAvailableCountries, ModalService } from '@vendure/admin-ui/core';
+
+import { AddressDetailDialogComponent } from '../address-detail-dialog/address-detail-dialog.component';
 
 @Component({
     selector: 'vdr-address-card',
@@ -9,7 +19,6 @@ import { CustomFieldConfig, GetAvailableCountries } from '@vendure/admin-ui/core
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AddressCardComponent implements OnInit {
-    editing = false;
     @Input() addressForm: FormGroup;
     @Input() customFields: CustomFieldConfig;
     @Input() availableCountries: GetAvailableCountries.Items[] = [];
@@ -18,10 +27,12 @@ export class AddressCardComponent implements OnInit {
     @Output() setAsDefaultShipping = new EventEmitter<string>();
     @Output() setAsDefaultBilling = new EventEmitter<string>();
 
+    constructor(private modalService: ModalService, private changeDetector: ChangeDetectorRef) {}
+
     ngOnInit(): void {
         const streetLine1 = this.addressForm.get('streetLine1') as FormControl;
         if (!streetLine1.value) {
-            this.editing = true;
+            this.editAddress();
         }
     }
 
@@ -41,5 +52,21 @@ export class AddressCardComponent implements OnInit {
     setAsDefaultShippingAddress() {
         this.setAsDefaultShipping.emit(this.addressForm.value.id);
         this.addressForm.markAsDirty();
+    }
+
+    editAddress() {
+        this.modalService
+            .fromComponent(AddressDetailDialogComponent, {
+                locals: {
+                    addressForm: this.addressForm,
+                    customFields: this.customFields,
+                    availableCountries: this.availableCountries,
+                },
+                size: 'md',
+                closable: true,
+            })
+            .subscribe(() => {
+                this.changeDetector.markForCheck();
+            });
     }
 }
