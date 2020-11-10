8cb2d0d6062e53746e2a4602fb569dcdfc7a1888
chladog <lukaschladek@gmail.com>
2020-11-10 09:42:40 +0100

    Export HistoryService

--- packages/core/src/service/index.ts
+++ packages/core/src/service/index.ts
@@ -35,4 +35,5 @@ export * from './services/tax-category.service';
 export * from './services/tax-rate.service';
 export * from './services/user.service';
 export * from './services/zone.service';
+export * from './services/history.service';
 export * from './transaction/transactional-connection';
