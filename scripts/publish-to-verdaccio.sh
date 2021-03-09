fcd86e3c5d2a2c64c4d403a9d158bba9681c5f67
Michael Bromley <michael@michaelbromley.co.uk>
2021-03-09 12:12:13 +0100

    chore: Remove pub-sub from verdaccio script

--- scripts/publish-to-verdaccio.sh
+++ scripts/publish-to-verdaccio.sh
@@ -21,7 +21,6 @@ cd ../core && npm publish -reg $VERDACCIO &&\
 cd ../create && npm publish -reg $VERDACCIO &&\
 cd ../elasticsearch-plugin && npm publish -reg $VERDACCIO &&\
 cd ../email-plugin && npm publish -reg $VERDACCIO &&\
-cd ../pub-sub-plugin && npm publish -reg $VERDACCIO &&\
 cd ../testing && npm publish -reg $VERDACCIO &&\
 cd ../ui-devkit && npm publish -reg $VERDACCIO &&\
 cd ../admin-ui/package && npm publish -reg $VERDACCIO
