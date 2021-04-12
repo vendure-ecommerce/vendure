63118f4c9f0790017e28d8f6e141243923ab4aaf
Kevin Mattutat <kevin.mattutat@spaceparrots.de>
2021-04-12 18:33:18 +0200

    test(core): added test case for translation import by file

--- packages/core/e2e/fixtures/test-plugins/translation-test-plugin.ts
+++ packages/core/e2e/fixtures/test-plugins/translation-test-plugin.ts
@@ -2,11 +2,18 @@ import { OnApplicationBootstrap } from '@nestjs/common';
 import { Args, Query, Resolver } from '@nestjs/graphql';
 import { Ctx, ErrorResult, I18nService, PluginCommonModule, RequestContext, VendurePlugin } from '@vendure/core';
 import gql from 'graphql-tag';
+import path from 'path';
 
 class CustomError extends ErrorResult {
     readonly __typename = 'CustomError';
     readonly errorCode = 'CUSTOM_ERROR';
     readonly message = 'CUSTOM_ERROR';
+}
+
+class NewError extends ErrorResult {
+    readonly __typename = 'NewError';
+    readonly errorCode = 'NEW_ERROR';
+    readonly message = 'NEW_ERROR';
 }
 
 @Resolver()
@@ -15,6 +22,11 @@ class TestResolver {
     @Query()
     async customErrorMessage(@Ctx() ctx: RequestContext, @Args() args: any) {
         return new CustomError();
+    }
+
+    @Query()
+    async newErrorMessage(@Ctx() ctx: RequestContext, @Args() args: any) {
+        return new NewError();
     }
 
 }
@@ -28,14 +40,21 @@ export const CUSTOM_ERROR_MESSAGE_TRANSLATION = 'A custom error message';
         schema: gql`
             extend type Query {
                 customErrorMessage: CustomResult
+                newErrorMessage: CustomResult
             }
+
             type CustomError implements ErrorResult {
                 errorCode: ErrorCode!
                 message: String!
             }
 
+            type NewError implements ErrorResult {
+                errorCode: ErrorCode!
+                message: String!
+            }
+
             "Return anything and the error that should be thrown"
-            union CustomResult = Product | CustomError
+            union CustomResult = Product | CustomError | NewError
         `,
         resolvers: [TestResolver],
     },
@@ -59,5 +78,7 @@ export class TranslationTestPlugin implements OnApplicationBootstrap {
             },
         });
 
+        this.i18nService.addTranslationFile('en', path.join(__dirname, '../i18n/en.json'))
+        this.i18nService.addTranslationFile('de', path.join(__dirname, '../i18n/de.json'))
     }
 }
--- packages/core/e2e/translations.e2e-spec.ts
+++ packages/core/e2e/translations.e2e-spec.ts
@@ -6,6 +6,8 @@ import path from 'path';
 import { initialData } from '../../../e2e-common/e2e-initial-data';
 import { testConfig, TEST_SETUP_TIMEOUT_MS } from '../../../e2e-common/test-config';
 
+import * as DE from './fixtures/i18n/de.json';
+import * as EN from './fixtures/i18n/en.json';
 import {
     CUSTOM_ERROR_MESSAGE_TRANSLATION,
     TranslationTestPlugin,
@@ -31,23 +33,50 @@ describe('Translation', () => {
         await server.destroy();
     });
 
-    it('shall receive custom error message', async () => {
-        const { customErrorMessage } = await adminClient.query(VERIFY_ERROR);
-        expect(customErrorMessage.errorCode).toBe('CUSTOM_ERROR');
-        expect(customErrorMessage.message).toBe(CUSTOM_ERROR_MESSAGE_TRANSLATION);
+    describe('translations added manualy', () => {
+        it('shall receive custom error message', async () => {
+            const { customErrorMessage } = await adminClient.query(CUSTOM_ERROR);
+            expect(customErrorMessage.errorCode).toBe('CUSTOM_ERROR');
+            expect(customErrorMessage.message).toBe(CUSTOM_ERROR_MESSAGE_TRANSLATION);
+        });
+
+        it('shall receive german error message', async () => {
+            const { customErrorMessage } = await adminClient.query(CUSTOM_ERROR, {}, { languageCode: LanguageCode.de });
+            expect(customErrorMessage.errorCode).toBe('CUSTOM_ERROR');
+            expect(customErrorMessage.message).toBe('DE_' + CUSTOM_ERROR_MESSAGE_TRANSLATION);
+        });
     });
 
-    it('shall receive german error message', async () => {
-        const { customErrorMessage } = await adminClient.query(VERIFY_ERROR, {}, { languageCode: LanguageCode.de });
-        expect(customErrorMessage.errorCode).toBe('CUSTOM_ERROR');
-        expect(customErrorMessage.message).toBe('DE_' + CUSTOM_ERROR_MESSAGE_TRANSLATION);
+    describe('translation added by file', () => {
+        it('shall receive custom error message', async () => {
+            const { newErrorMessage } = await adminClient.query(NEW_ERROR);
+            expect(newErrorMessage.errorCode).toBe('NEW_ERROR');
+            expect(newErrorMessage.message).toBe(EN.errorResult.NEW_ERROR);
+        });
+
+        it('shall receive german error message', async () => {
+            const { newErrorMessage } = await adminClient.query(NEW_ERROR, {}, { languageCode: LanguageCode.de });
+            expect(newErrorMessage.errorCode).toBe('NEW_ERROR');
+            expect(newErrorMessage.message).toBe(DE.errorResult.NEW_ERROR);
+        });
     });
 
 });
 
-const VERIFY_ERROR = gql`
+const CUSTOM_ERROR = gql`
     query CustomError {
         customErrorMessage {
+            ... on ErrorResult {
+                errorCode
+                message
+            }
+        }
+    }
+`;
+
+const NEW_ERROR = gql`
+    query NewError {
+        newErrorMessage {
             ... on ErrorResult {
                 errorCode
                 message
