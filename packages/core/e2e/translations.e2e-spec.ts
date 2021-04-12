13a4b68800e96df3e6037414a4a5fa60145d0b79
Drayke <officialdrayke@outlook.de>
2021-04-12 21:59:12 +0200

    feat(core): Add server translations via plugin application bootstrap
    Co-authored-by: Kevin Mattutat <kevin.mattutat@spaceparrots.de>
    
    Closes #810

---
+++ packages/core/e2e/translations.e2e-spec.ts
@@ -0,0 +1,86 @@
+import { LanguageCode, mergeConfig } from '@vendure/core';
+import { createTestEnvironment } from '@vendure/testing';
+import gql from 'graphql-tag';
+import path from 'path';
+
+import { initialData } from '../../../e2e-common/e2e-initial-data';
+import { testConfig, TEST_SETUP_TIMEOUT_MS } from '../../../e2e-common/test-config';
+
+import * as DE from './fixtures/i18n/de.json';
+import * as EN from './fixtures/i18n/en.json';
+import {
+    CUSTOM_ERROR_MESSAGE_TRANSLATION,
+    TranslationTestPlugin,
+} from './fixtures/test-plugins/translation-test-plugin';
+
+describe('Translation', () => {
+    const { server, adminClient } = createTestEnvironment(
+        mergeConfig(testConfig, {
+            plugins: [TranslationTestPlugin],
+        }),
+    );
+
+    beforeAll(async () => {
+        await server.init({
+            initialData,
+            productsCsvPath: path.join(__dirname, 'fixtures/e2e-products-minimal.csv'),
+            customerCount: 0,
+        });
+        await adminClient.asSuperAdmin();
+    }, TEST_SETUP_TIMEOUT_MS);
+
+    afterAll(async () => {
+        await server.destroy();
+    });
+
+    describe('translations added manualy', () => {
+        it('shall receive custom error message', async () => {
+            const { customErrorMessage } = await adminClient.query(CUSTOM_ERROR);
+            expect(customErrorMessage.errorCode).toBe('CUSTOM_ERROR');
+            expect(customErrorMessage.message).toBe(CUSTOM_ERROR_MESSAGE_TRANSLATION);
+        });
+
+        it('shall receive german error message', async () => {
+            const { customErrorMessage } = await adminClient.query(CUSTOM_ERROR, {}, { languageCode: LanguageCode.de });
+            expect(customErrorMessage.errorCode).toBe('CUSTOM_ERROR');
+            expect(customErrorMessage.message).toBe('DE_' + CUSTOM_ERROR_MESSAGE_TRANSLATION);
+        });
+    });
+
+    describe('translation added by file', () => {
+        it('shall receive custom error message', async () => {
+            const { newErrorMessage } = await adminClient.query(NEW_ERROR);
+            expect(newErrorMessage.errorCode).toBe('NEW_ERROR');
+            expect(newErrorMessage.message).toBe(EN.errorResult.NEW_ERROR);
+        });
+
+        it('shall receive german error message', async () => {
+            const { newErrorMessage } = await adminClient.query(NEW_ERROR, {}, { languageCode: LanguageCode.de });
+            expect(newErrorMessage.errorCode).toBe('NEW_ERROR');
+            expect(newErrorMessage.message).toBe(DE.errorResult.NEW_ERROR);
+        });
+    });
+
+});
+
+const CUSTOM_ERROR = gql`
+    query CustomError {
+        customErrorMessage {
+            ... on ErrorResult {
+                errorCode
+                message
+            }
+        }
+    }
+`;
+
+const NEW_ERROR = gql`
+    query NewError {
+        newErrorMessage {
+            ... on ErrorResult {
+                errorCode
+                message
+            }
+        }
+    }
+`;
--- packages/core/src/i18n/i18n.service.ts
+++ packages/core/src/i18n/i18n.service.ts
@@ -1,16 +1,30 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { Handler, Request } from 'express';
+import * as fs from 'fs';
 import { GraphQLError } from 'graphql';
 import i18next, { TFunction } from 'i18next';
 import i18nextMiddleware from 'i18next-express-middleware';
+import Backend from 'i18next-fs-backend';
 import ICU from 'i18next-icu';
-import Backend from 'i18next-node-fs-backend';
 import path from 'path';
 
 import { GraphQLErrorResult } from '../common/error/error-result';
+import { Logger } from '../config';
 import { ConfigService } from '../config/config.service';
 
 import { I18nError } from './i18n-error';
+
+/**
+ * @description
+ * I18n resources used for translations
+ *
+ * @docsCategory Translation
+ */
+export interface VendureTranslationResources {
+    error: any;
+    errorResult: any;
+    message: any;
+}
 
 export interface I18nRequest extends Request {
     t: TFunction;
@@ -21,11 +35,19 @@ export interface I18nRequest extends Request {
  * The `i18next-express-middleware` middleware detects the client's preferred language based on
  * the `Accept-Language` header or "lang" query param and adds language-specific translation
  * functions to the Express request / response objects.
+ * @docsCategory Translation
  */
 @Injectable()
 export class I18nService implements OnModuleInit {
+    /**
+     * @internal
+     * @param configService
+     */
     constructor(private configService: ConfigService) {}
 
+    /**
+     * @internal
+     */
     onModuleInit() {
         return i18next
             .use(i18nextMiddleware.LanguageDetector)
@@ -35,7 +57,7 @@ export class I18nService implements OnModuleInit {
                 preload: ['en', 'de'],
                 fallbackLng: 'en',
                 detection: {
-                    lookupQuerystring: 'lang',
+                    lookupQuerystring: 'languageCode',
                 },
                 backend: {
                     loadPath: path.join(__dirname, 'messages/{{lng}}.json'),
@@ -44,12 +66,44 @@ export class I18nService implements OnModuleInit {
             });
     }
 
+    /**
+     * @internal
+     */
     handle(): Handler {
         return i18nextMiddleware.handle(i18next);
     }
 
     /**
+     * @description
+     * Add a I18n translation by json file
+     *
+     * @param langKey language key of the I18n translation file
+     * @param filePath path to the I18n translation file
+     */
+    addTranslationFile(langKey: string, filePath: string): void {
+        try {
+            const rawData = fs.readFileSync(filePath);
+            const resources = JSON.parse(rawData.toString('utf-8'));
+            this.addTranslation(langKey, resources);
+        } catch (err) {
+            Logger.error(`Could not load resources file ${filePath}`, `I18nService`);
+        }
+    }
+
+    /**
+     * @description
+     * Add a I18n translation (key-value) resource
+     *
+     * @param langKey language key of the I18n translation file
+     * @param resources key-value translations
+     */
+    addTranslation(langKey: string, resources: VendureTranslationResources | any): void {
+        i18next.addResourceBundle(langKey, 'translation', resources, true, true);
+    }
+
+    /**
      * Translates the originalError if it is an instance of I18nError.
+     * @internal
      */
     translateError(req: I18nRequest, error: GraphQLError) {
         const originalError = error.originalError;
@@ -73,6 +127,7 @@ export class I18nService implements OnModuleInit {
 
     /**
      * Translates the message of an ErrorResult
+     * @internal
      */
     translateErrorResult(req: I18nRequest, error: GraphQLErrorResult) {
         const t: TFunction = req.t;
