a8ca0197dee97dfe7298353be4ddafc666434397
Wanztwurst <github@crystal-mett.de>
2021-04-22 17:13:08 +0200

    feat(core): Improved translations
    Closes #839

--- packages/core/src/service/services/product-variant.service.ts
+++ packages/core/src/service/services/product-variant.service.ts
@@ -723,7 +723,7 @@ export class ProductVariantService {
                 const variantOptionIds = this.sortJoin(variant.options, ',', 'id');
                 if (variantOptionIds === inputOptionIds) {
                     throw new UserInputError('error.product-variant-options-combination-already-exists', {
-                        optionNames: this.sortJoin(variant.options, ', ', 'code'),
+                        variantName: translateDeep(variant, ctx.languageCode).name,
                     });
                 }
             });
