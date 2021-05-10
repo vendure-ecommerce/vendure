9d57c1a184d5b12853c484dae66416191bc6c0d4
Michael Bromley <michael@michaelbromley.co.uk>
2021-05-10 15:39:20 +0200

    chore(core): Remove deprecated EventBus.subscribe() method

--- packages/core/src/event-bus/event-bus.ts
+++ packages/core/src/event-bus/event-bus.ts
@@ -8,9 +8,6 @@ import { TRANSACTION_MANAGER_KEY } from '../common/constants';
 
 import { VendureEvent } from './vendure-event';
 
-export type EventHandler<T extends VendureEvent> = (event: T) => void;
-export type UnsubscribeFn = () => void;
-
 /**
  * @description
  * The EventBus is used to globally publish events which can then be subscribed to.
@@ -19,7 +16,6 @@ export type UnsubscribeFn = () => void;
  * */
 @Injectable()
 export class EventBus implements OnModuleDestroy {
-    private subscriberMap = new Map<Type<VendureEvent>, Array<EventHandler<any>>>();
     private eventStream = new Subject<VendureEvent>();
     private destroy$ = new Subject();
 
@@ -28,14 +24,6 @@ export class EventBus implements OnModuleDestroy {
      * Publish an event which any subscribers can react to.
      */
     publish<T extends VendureEvent>(event: T): void {
-        const eventType = (event as any).constructor;
-        const handlers = this.subscriberMap.get(eventType);
-        if (handlers) {
-            const length = handlers.length;
-            for (let i = 0; i < length; i++) {
-                handlers[i](event);
-            }
-        }
         this.eventStream.next(this.prepareRequestContext(event));
     }
 
@@ -48,28 +36,6 @@ export class EventBus implements OnModuleDestroy {
             takeUntil(this.destroy$),
             filter(e => (e as any).constructor === type),
         ) as Observable<T>;
-    }
-
-    /**
-     * @description
-     * Deprecated: use `ofType()` instead.
-     *
-     * Subscribe to the given event type. Returns an unsubscribe function which can be used
-     * to unsubscribe the handler from the event.
-     *
-     * @deprecated
-     */
-    subscribe<T extends VendureEvent>(type: Type<T>, handler: EventHandler<T>): UnsubscribeFn {
-        const handlers = this.subscriberMap.get(type) || [];
-        if (!handlers.includes(handler)) {
-            handlers.push(handler);
-        }
-        this.subscriberMap.set(type, handlers);
-        return () =>
-            this.subscriberMap.set(
-                type,
-                handlers.filter(h => h !== handler),
-            );
     }
 
     /** @internal */
