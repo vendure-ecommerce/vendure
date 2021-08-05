35f47c4cbda7d2f6193fb28668c54461cf10a36d
Michael Bromley <michael@michaelbromley.co.uk>
2021-08-05 10:20:59 +0200

    docs(core): Fix typo in docs tag

--- packages/core/src/config/vendure-config.ts
+++ packages/core/src/config/vendure-config.ts
@@ -348,7 +348,7 @@ export interface AuthOptions {
      * `password` property - doing so will result in an error. Instead, the password is set at a later stage
      * (once the email with the verification token has been opened) via the `verifyCustomerAccount` mutation.
      *
-     * @defaut true
+     * @default true
      */
     requireVerification?: boolean;
     /**
