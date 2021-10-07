3a68da9b745ad0fc32a7a2dadc167a87eaf5ff01
Michael Bromley <michael@michaelbromley.co.uk>
2021-10-07 22:21:37 +0200

    refactor(core): Rename some args

--- packages/core/src/job-queue/job-buffer/job-buffer-storage-strategy.ts
+++ packages/core/src/job-queue/job-buffer/job-buffer-storage-strategy.ts
@@ -2,7 +2,7 @@ import { InjectableStrategy } from '../../common/types/injectable-strategy';
 import { Job } from '../job';
 
 export interface JobBufferStorageStrategy extends InjectableStrategy {
-    add(processorId: string, job: Job): Promise<Job>;
+    add(bufferId: string, job: Job): Promise<Job>;
     bufferSize(bufferIds?: string[]): Promise<{ [bufferId: string]: number }>;
     flush(bufferIds?: string[]): Promise<{ [bufferId: string]: Job[] }>;
 }
