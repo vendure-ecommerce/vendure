b16e9f68a228022084248a4cc07f4512324c2e34
Kevin <kevin@fainin.com>
2021-11-15 16:05:12 +0100

    feat(core): Implemented AssetEvent with new entity event base class (#1219)

--- packages/core/src/event-bus/events/asset-event.ts
+++ packages/core/src/event-bus/events/asset-event.ts
@@ -1,21 +1,37 @@
-import { RequestContext } from '../../api/common/request-context';
+import { CreateAssetInput, DeleteAssetInput, UpdateAssetInput } from '@vendure/common/lib/generated-types';
+
+import { RequestContext } from '../../api';
 import { Asset } from '../../entity';
-import { VendureEvent } from '../vendure-event';
+import { VendureEntityEvent } from '../vendure-entity-event';
+
+type AssetInputTypes = CreateAssetInput | UpdateAssetInput | DeleteAssetInput;
 
 /**
  * @description
  * This event is fired whenever aa {@link Asset} is added, updated
- * or deleted.
+ * or deleted. The `input` property is only defined for `'created'` & `'updated'` event types.
  *
  * @docsCategory events
  * @docsPage Event Types
+ * @since 1.4
  */
-export class AssetEvent extends VendureEvent {
+export class AssetEvent extends VendureEntityEvent<Asset, AssetInputTypes> {
     constructor(
         public ctx: RequestContext,
-        public asset: Asset,
+        public entity: Asset,
         public type: 'created' | 'updated' | 'deleted',
+        public input?: AssetInputTypes,
     ) {
-        super();
+        super(entity, type, ctx, input);
+    }
+
+    /**
+     * Return an asset field to become compatible with the
+     * deprecated old version of AssetEvent
+     * @deprecated Use `entity` instead
+     * @since 1.4
+     */
+    get asset(): Asset {
+        return this.entity;
     }
 }
