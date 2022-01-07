704eabe8ae333bc3062577769995062e8248e86f
Michael Bromley <michael@michaelbromley.co.uk>
2022-01-07 17:17:12 +0000

    docs(core): Add docs to AssetService

--- packages/core/src/entity/asset/orderable-asset.entity.ts
+++ packages/core/src/entity/asset/orderable-asset.entity.ts
@@ -6,11 +6,14 @@ import { Asset } from '../asset/asset.entity';
 import { VendureEntity } from '../base/base.entity';
 
 /**
+ * @description
  * This base class is extended in order to enable specific ordering of the one-to-many
  * Entity -> Assets relation. Using a many-to-many relation does not provide a way
  * to guarantee order of the Assets, so this entity is used in place of the
  * usual join table that would be created by TypeORM.
  * See https://typeorm.io/#/many-to-many-relations/many-to-many-relations-with-custom-properties
+ *
+ * @docsCategory entities
  */
 export abstract class OrderableAsset extends VendureEntity implements Orderable {
     protected constructor(input?: DeepPartial<OrderableAsset>) {
@@ -20,7 +23,7 @@ export abstract class OrderableAsset extends VendureEntity implements Orderable 
     @Column()
     assetId: ID;
 
-    @ManyToOne((type) => Asset, { eager: true, onDelete: 'CASCADE' })
+    @ManyToOne(type => Asset, { eager: true, onDelete: 'CASCADE' })
     asset: Asset;
 
     @Column()
