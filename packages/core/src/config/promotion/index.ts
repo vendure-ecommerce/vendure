56b30fab35c8a3065631a385b82c26cb16545c7b
Michael Bromley <michael@michaelbromley.co.uk>
2022-03-11 15:44:20 +0100

    fix(core): Export all Promotion conditions & actions
    Relates to #1308

--- packages/core/src/config/promotion/index.ts
+++ packages/core/src/config/promotion/index.ts
@@ -15,10 +15,14 @@ export * from './promotion-condition';
 export * from './actions/facet-values-percentage-discount-action';
 export * from './actions/order-percentage-discount-action';
 export * from './actions/product-percentage-discount-action';
+export * from './actions/free-shipping-action';
+export * from './actions/buy-x-get-y-free-action';
+export * from './actions/order-fixed-discount-action';
 export * from './conditions/has-facet-values-condition';
 export * from './conditions/min-order-amount-condition';
 export * from './conditions/contains-products-condition';
 export * from './conditions/customer-group-condition';
+export * from './conditions/buy-x-get-y-free-condition';
 export * from './utils/facet-value-checker';
 
 export const defaultPromotionActions = [
