0aa90af86d2e2c612af58cdbcd1122de808f5dce
Daniel Stehlik <daniel.stehlik@rheelly.net>
2022-03-17 16:45:32 +0100

    fix: Falsy, no-list customFields are now undefined
    CustomFields that are falsy and not a list will now be handled
    as undefined instead of null to ensure no errors are thrown for
    notNullable customFields

--- packages/core/src/data-import/providers/importer/importer.ts
+++ packages/core/src/data-import/providers/importer/importer.ts
@@ -356,13 +356,13 @@ export class Importer {
     }
 
     private processCustomFieldValues(customFields: { [field: string]: string }, config: CustomFieldConfig[]) {
-        const processed: { [field: string]: string | string[] | null } = {};
+        const processed: { [field: string]: string | string[] | undefined } = {};
         for (const fieldDef of config) {
             const value = customFields[fieldDef.name];
             if (fieldDef.list === true) {
                 processed[fieldDef.name] = value ? value?.split('|').filter(val => val.trim() !== '') : [];
             } else {
-                processed[fieldDef.name] = value ? value : null;
+                processed[fieldDef.name] = value ? value : undefined;
             }
         }
         return processed;
