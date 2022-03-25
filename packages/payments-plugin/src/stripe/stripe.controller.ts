4d55949b052f900e2221b681a40908bd8a3a77d5
Julien <julien@skydeals.shop>
2022-03-25 09:18:49 +0100

    fix(payments-plugin): Send 200 response from Stripe webhook (#1487)
    Without a 200 response, Stripe will continue to retry the webhook endpoint.

--- packages/payments-plugin/src/stripe/stripe.controller.ts
+++ packages/payments-plugin/src/stripe/stripe.controller.ts
@@ -116,6 +116,7 @@ export class StripeController {
         }
 
         Logger.info(`Stripe payment intent id ${paymentIntent.id} added to order ${orderCode}`, loggerCtx);
+        response.status(HttpStatus.OK).send('Ok');
     }
 
     private async createContext(channelToken: string): Promise<RequestContext> {
