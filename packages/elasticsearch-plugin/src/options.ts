e75ea9ca192a13cd039aefe08a8d1f26e922bae5
Michael Bromley <michael@michaelbromley.co.uk>
2022-04-01 12:18:15 +0200

    docs(elasticsearch-plugin): Add link to stemming example

--- packages/elasticsearch-plugin/src/options.ts
+++ packages/elasticsearch-plugin/src/options.ts
@@ -92,6 +92,8 @@ export interface ElasticsearchOptions {
      *   }
      * },
      * ```
+     * A more complete example can be found in the discussion thread
+     * [How to make elastic plugin to search by substring with stemming](https://github.com/vendure-ecommerce/vendure/discussions/1066).
      *
      * @since 1.2.0
      * @default
