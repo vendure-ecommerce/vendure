73da4642076d2ddfaa5961f5b1fe9b990e631b5f
Michael Bromley <michael@michaelbromley.co.uk>
2022-04-14 14:43:38 +0200

    chore(core): Add issue reference

--- packages/core/src/config/entity-metadata/add-foreign-key-indices.ts
+++ packages/core/src/config/entity-metadata/add-foreign-key-indices.ts
@@ -11,6 +11,8 @@ import { EntityMetadataModifier } from './entity-metadata-modifier';
  * by default in MySQL/MariaDB, but not in Postgres. So this modification can lead to improved
  * performance with Postgres - especially when dealing with large numbers of products, orders etc.
  *
+ * See https://github.com/vendure-ecommerce/vendure/issues/1502
+ *
  * TODO: In v2 we will add the Index to all relations manually, this making this redundant.
  */
 export const addForeignKeyIndices: EntityMetadataModifier = (metadata: MetadataArgsStorage) => {
