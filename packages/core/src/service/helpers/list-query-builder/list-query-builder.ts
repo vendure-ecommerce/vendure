47b54e68dae8f298a73d3e74ad13730bc3a49ef0
Michael Bromley <michael@michaelbromley.co.uk>
2022-06-01 13:59:11 +0200

    chore: Refactor handling of list `take` arg

--- packages/core/src/service/helpers/list-query-builder/list-query-builder.ts
+++ packages/core/src/service/helpers/list-query-builder/list-query-builder.ts
@@ -277,7 +277,7 @@ export class ListQueryBuilder implements OnApplicationBootstrap {
         const rawConnection = this.connection.rawConnection;
         const skip = Math.max(options.skip ?? 0, 0);
         // `take` must not be negative, and must not be greater than takeLimit
-        let take = Math.min(Math.max(options.take ?? 0, 0), takeLimit) || takeLimit;
+        let take = options.take == null ? takeLimit : Math.min(Math.max(options.take, 0), takeLimit);
         if (options.skip !== undefined && options.take === undefined) {
             take = takeLimit;
         }
