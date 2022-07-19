c8e0f762f9dc776e77b42c82107e81b5c1a96658
Michael Bromley <michael@michaelbromley.co.uk>
2022-07-19 16:26:31 +0200

    chore(job-queue-plugin): Update dependencies for BullMQ

--- packages/job-queue-plugin/src/bullmq/constants.ts
+++ packages/job-queue-plugin/src/bullmq/constants.ts
@@ -1,7 +1,9 @@
+import { JobType } from 'bullmq';
+
 export const loggerCtx = 'BullMQJobQueuePlugin';
 export const BULLMQ_PLUGIN_OPTIONS = Symbol('BULLMQ_PLUGIN_OPTIONS');
 
-export const ALL_JOB_TYPES = [
+export const ALL_JOB_TYPES: JobType[] = [
     'completed',
     'failed',
     'delayed',
