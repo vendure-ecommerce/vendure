aa13dcb5192de39fb1a0baaeddadf82cb51b25f3
Michael Bromley <michael@michaelbromley.co.uk>
2022-09-06 16:27:05 +0200

    fix(admin-ui): Fix issues with rich text editor in custom field

--- packages/admin-ui/src/lib/core/src/shared/components/rich-text-editor/prosemirror/context-menu/context-menu.service.ts
+++ packages/admin-ui/src/lib/core/src/shared/components/rich-text-editor/prosemirror/context-menu/context-menu.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, combineLatest, Observable, Subject, of, interval } from 'rxjs';
+import { BehaviorSubject, combineLatest, interval, Observable, of, Subject } from 'rxjs';
 import {
     bufferWhen,
     debounceTime,
@@ -77,6 +77,6 @@ export class ContextMenuService {
     }
 
     clearContextMenu() {
-        this.setContextMenuConfig$.next();
+        this.setContextMenuConfig$.next(undefined);
     }
 }
