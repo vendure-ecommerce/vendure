3d4a4a80abf4a60c2b1b4cf2104f3ad0dd4dfcd3
Martijn <martijn.brug@gmail.com>
2022-11-18 15:17:49 +0100

    feat(payments-plugin): logger in e2e

--- packages/payments-plugin/e2e/mollie-payment.e2e-spec.ts
+++ packages/payments-plugin/e2e/mollie-payment.e2e-spec.ts
@@ -102,7 +102,6 @@ describe('Mollie payments', () => {
     beforeAll(async () => {
         const devConfig = mergeConfig(testConfig(), {
             plugins: [MolliePlugin.init({ vendureHost: mockData.host })],
-            logger: new DefaultLogger({level: LogLevel.Verbose})
         });
         const env = createTestEnvironment(devConfig);
         serverPort = devConfig.apiOptions.port;
