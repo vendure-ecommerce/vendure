88b042f1126e97e67a151b4f8d90a62ba867e000
AhmedNishad <ahmednishad3@gmail.com>
2022-12-08 12:37:10 +0530

    chore: reverted dev config

--- packages/dev-server/dev-config.ts
+++ packages/dev-server/dev-config.ts
@@ -295,7 +295,7 @@ function getDbConfig(): ConnectionOptions {
                 synchronize: true,
                 type: 'mysql',
                 host: 'localhost',
-                port: 3307,
+                port: 3306,
                 username: 'root',
                 password: 'root',
                 database: 'vendure-dev',
