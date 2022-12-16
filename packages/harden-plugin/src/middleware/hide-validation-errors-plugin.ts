9df8164929344a81e5efa955176b7ef36b623805
Michael Bromley <michael@michaelbromley.co.uk>
2022-12-16 14:27:38 +0100

    chore: Create HardenPlugin

---
+++ packages/harden-plugin/src/middleware/hide-validation-errors-plugin.ts
@@ -0,0 +1,26 @@
+import { ValidationError } from 'apollo-server-core';
+import { ApolloServerPlugin, GraphQLRequestListener, GraphQLServiceContext } from 'apollo-server-plugin-base';
+
+/**
+ * @description
+ * Hides graphql-js suggestions when invalid field names are given.
+ * Based on ideas discussed in https://github.com/apollographql/apollo-server/issues/3919
+ */
+export class HideValidationErrorsPlugin implements ApolloServerPlugin {
+    requestDidStart(): GraphQLRequestListener {
+        return {
+            willSendResponse: async requestContext => {
+                const { errors, context } = requestContext;
+                if (errors) {
+                    (requestContext.response as any).errors = errors.map(err => {
+                        if (err.message.includes('Did you mean')) {
+                            return new ValidationError('Invalid request');
+                        } else {
+                            return err;
+                        }
+                    });
+                }
+            },
+        };
+    }
+}
