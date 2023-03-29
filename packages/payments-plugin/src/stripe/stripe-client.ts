7d82dd5f7d6854a804027561de71fa3b052ed344
Martijn <martijn.brug@gmail.com>
2023-03-29 09:32:35 +0200

    feat(payment-plugin): Pr fix

--- packages/payments-plugin/src/stripe/stripe-client.ts
+++ packages/payments-plugin/src/stripe/stripe-client.ts
@@ -4,14 +4,9 @@ import Stripe from 'stripe';
  * Wrapper around the Stripe client that exposes ApiKey and WebhookSecret
  */
 export class VendureStripeClient extends Stripe {
-
-    constructor(
-        public apiKey: string,
-        public webhookSecret: string
-    ) {
+    constructor(private apiKey: string, public webhookSecret: string) {
         super(apiKey, {
             apiVersion: null as any, // Use accounts default version
         });
     }
-
 }
