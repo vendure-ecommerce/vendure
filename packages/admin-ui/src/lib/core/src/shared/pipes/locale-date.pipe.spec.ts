2c6121d0ff0a9feda8aac8bd8748d574b1217255
Michael Bromley <michael@michaelbromley.co.uk>
2023-04-04 10:23:24 +0200

    chore: Fix testing issues

--- packages/admin-ui/src/lib/core/src/shared/pipes/locale-date.pipe.spec.ts
+++ packages/admin-ui/src/lib/core/src/shared/pipes/locale-date.pipe.spec.ts
@@ -6,15 +6,15 @@ describe('LocaleDatePipe', () => {
     const testDate = new Date('2021-01-12T09:12:42');
     it('medium format', () => {
         const pipe = new LocaleDatePipe();
-        expect(pipe.transform(testDate, 'medium', LanguageCode.en)).toBe('Jan 12, 2021, 9:12:42 AM');
+        expect(pipe.transform(testDate, 'medium', LanguageCode.en)).toBe('Jan 12, 2021, 9:12:42 AM');
     });
     it('mediumTime format', () => {
         const pipe = new LocaleDatePipe();
-        expect(pipe.transform(testDate, 'mediumTime', LanguageCode.en)).toBe('9:12:42 AM');
+        expect(pipe.transform(testDate, 'mediumTime', LanguageCode.en)).toBe('9:12:42 AM');
     });
     it('short format', () => {
         const pipe = new LocaleDatePipe();
-        expect(pipe.transform(testDate, 'short', LanguageCode.en)).toBe('1/12/21, 9:12 AM');
+        expect(pipe.transform(testDate, 'short', LanguageCode.en)).toBe('1/12/21, 9:12 AM');
     });
     it('longDate format', () => {
         const pipe = new LocaleDatePipe();
@@ -23,7 +23,7 @@ describe('LocaleDatePipe', () => {
 
     it('medium format German', () => {
         const pipe = new LocaleDatePipe();
-        expect(pipe.transform(testDate, 'medium', LanguageCode.de)).toBe('12. Jan. 2021, 9:12:42 AM');
+        expect(pipe.transform(testDate, 'medium', LanguageCode.de)).toBe('12. Jan. 2021, 9:12:42 AM');
     });
 
     it('medium format Chinese', () => {
