8c41f3351151a129b0a383462ce96df05cb7c708
Michael Bromley <michael@michaelbromley.co.uk>
2023-04-20 17:39:47 +0200

    feat(admin-ui): Style left nav surface

--- packages/admin-ui/src/lib/core/src/components/main-nav/main-nav.component.ts
+++ packages/admin-ui/src/lib/core/src/components/main-nav/main-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { NavMenuSection } from '../../providers/nav-builder/nav-builder-types';
@@ -10,13 +10,37 @@ import { BaseNavComponent } from '../base-nav/base-nav.component';
     styleUrls: ['./main-nav.component.scss'],
 })
 export class MainNavComponent extends BaseNavComponent implements OnInit {
+    @Input() displayMode: string | undefined;
     mainMenuConfig$: Observable<NavMenuSection[]>;
+    expandedSections: string[] = [];
 
     override ngOnInit(): void {
         super.ngOnInit();
 
         this.mainMenuConfig$ = this.navBuilderService.menuConfig$.pipe(
-            map(sections => sections.filter(s => s.displayMode === 'regular' || !s.displayMode)),
+            map(sections =>
+                sections.filter(s =>
+                    this.displayMode ? s.displayMode === this.displayMode : !s.displayMode,
+                ),
+            ),
         );
+    }
+
+    toggleExpand(section: NavMenuSection) {
+        if (this.expandedSections.includes(section.id)) {
+            this.expandedSections = this.expandedSections.filter(id => id !== section.id);
+        } else {
+            this.expandedSections.push(section.id);
+        }
+    }
+
+    getStyleForSection(section: NavMenuSection) {
+        if (section.collapsible) {
+            if (this.expandedSections.includes(section.id)) {
+                return { maxHeight: `${section.items.length * 33}px`, opacity: 1, visibility: 'visible' };
+            } else {
+                return { maxHeight: '0px', opacity: 0, visibility: 'hidden' };
+            }
+        }
     }
 }
