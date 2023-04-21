0cc20f227822fb2b6a0d99fc7e62fc14b16c3b5b
Mizan3050 <43161459+Mizan3050@users.noreply.github.com>
2023-04-21 13:31:41 +0530

    feat(admin-ui): Search field added on administrators list on dashboard -> administrator. (#2130)

--- packages/admin-ui/src/lib/settings/src/components/administrator-list/administrator-list.component.ts
+++ packages/admin-ui/src/lib/settings/src/components/administrator-list/administrator-list.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { marker as _ } from '@biesbjerg/ngx-translate-extract-marker';
 import {
     BaseListComponent,
     DataService,
     GetAdministrators,
+    LogicalOperator,
     ModalService,
     NotificationService,
+    SortOrder,
 } from '@vendure/admin-ui/core';
-import { EMPTY } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { EMPTY, merge } from 'rxjs';
+import { debounceTime, filter, switchMap, takeUntil } from 'rxjs/operators';
 
 @Component({
     selector: 'vdr-administrator-list',
@@ -19,7 +22,8 @@ import { switchMap } from 'rxjs/operators';
 export class AdministratorListComponent extends BaseListComponent<
     GetAdministrators.Query,
     GetAdministrators.Items
-> {
+> implements OnInit {
+    searchControl = new FormControl('');
     constructor(
         private dataService: DataService,
         router: Router,
@@ -31,7 +35,21 @@ export class AdministratorListComponent extends BaseListComponent<
         super.setQueryFn(
             (...args: any[]) => this.dataService.administrator.getAdministrators(...args),
             (data) => data.administrators,
+            (skip, take) => this.createSearchQuery(skip, take, this.searchControl.value)
         );
+    }
+
+    ngOnInit() {
+        super.ngOnInit();
+        const searchTerms$ = merge(this.searchControl.valueChanges).pipe(
+            filter(value => 2 < value.length || value.length === 0),
+            debounceTime(250),
+        );
+        merge(searchTerms$, this.route.queryParamMap)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe(val => {
+            this.refresh();
+        });
     }
 
     deleteAdministrator(administrator: GetAdministrators.Items) {
@@ -62,5 +80,41 @@ export class AdministratorListComponent extends BaseListComponent<
                     });
                 },
             );
+    }
+
+    createSearchQuery(
+        skip: number,
+        take: number,
+        searchTerm: string) {
+        let filter = {};
+        let filterOperator: LogicalOperator = LogicalOperator.AND;
+
+        if (searchTerm) {
+            filter = {
+                emailAddress: {
+                    contains: searchTerm,
+                },
+                firstName: {
+                    contains: searchTerm,
+                },
+                lastName: {
+                    contains: searchTerm,
+                },
+            };
+            filterOperator = LogicalOperator.OR;
+        }
+        return {
+            options: {
+                skip,
+                take,
+                filter: {
+                    ...(filter ?? {}),
+                },
+                sort: {
+                    updatedAt: SortOrder.DESC,
+                },
+                filterOperator,
+            },
+        };
     }
 }
