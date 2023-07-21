2b698268703614140eee65d49561e7711fe06016
Michael Bromley <michael@michaelbromley.co.uk>
2023-07-21 12:38:55 +0200

    docs: Styling & branding

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -57,10 +57,10 @@ const config = {
             // Replace with your project's social card
             image: 'img/docusaurus-social-card.jpg',
             navbar: {
-                title: 'My Site',
+                title: '',
                 logo: {
-                    alt: 'My Site Logo',
-                    src: 'img/logo.svg',
+                    alt: 'Vendure logo',
+                    src: 'img/logo.webp',
                 },
                 items: [
                     {
