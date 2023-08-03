dffed9bb726103c71dbc2dc7ccb4795daa0c313e
Michael Bromley <michael@michaelbromley.co.uk>
2023-08-03 09:35:50 +0200

    docs: Improve sidebar styling

---
+++ docs/src/theme/DocSidebarItem/Link/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Link from '@theme-original/DocSidebarItem/Link';
+
+export default function LinkWrapper(props) {
+    return (
+        <>
+            <Link {...props} />
+        </>
+    );
+}
