39b19cc8b7609f2064746a6853b31a3afb0c9289
Michael Bromley <michael@michaelbromley.co.uk>
2023-08-04 14:52:23 +0200

    docs: Add api client examples

---
+++ docs/src/components/Stackblitz/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Stackblitz(props: { id: string }) {
+    return (
+        <iframe
+            style={{
+                width: '100%',
+                minHeight: '500px',
+            }}
+            src={`https://stackblitz.com/edit/${props.id}?ctl=1&embed=1`}
+        />
+    );
+}
