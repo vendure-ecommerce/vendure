d01768ab12c1d9b216b3bf10c6a5e494771f8ec8
Pieter Doms <d0ms_007@hotmail.com>
2023-08-24 19:49:10 +0200

    fix: add some tests for nullable types

--- packages/admin-ui/src/lib/core/src/common/utilities/create-updated-translatable.spec.ts
+++ packages/admin-ui/src/lib/core/src/common/utilities/create-updated-translatable.spec.ts
@@ -189,6 +189,8 @@ describe('createUpdatedTranslatable()', () => {
             { name: 'c', type: 'float', list: false, nullable: false },
             { name: 'd', type: 'datetime', list: false, nullable: false },
             { name: 'e', type: 'string', list: false, nullable: false },
+            { name: 'f', type: 'int', list: false, nullable: true },
+            { name: 'g', type: 'datetime', list: false, nullable: true },
         ];
 
         const formValue = {
@@ -198,6 +200,8 @@ describe('createUpdatedTranslatable()', () => {
                 c: '',
                 d: '',
                 e: '',
+                f: '',
+                g: '',
             },
         };
 
@@ -213,5 +217,7 @@ describe('createUpdatedTranslatable()', () => {
         expect(result.customFields.c).toBe(0);
         expect(result.customFields.d instanceof Date).toBe(true);
         expect(result.customFields.e).toBe('');
+        expect(result.customFields.e).toBe(null);
+        expect(result.customFields.g).toBe(null);
     });
 });
