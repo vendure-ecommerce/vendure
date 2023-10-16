cd16726eb0a63360e56dad5545cbe2af2f0ad4da
Balázs Gallay <balazs.gallay@zooshgroup.com>
2023-10-16 14:46:01 +0200

    fix(payments-plugin): Fix stripe raw-body parsing issue

--- packages/payments-plugin/src/stripe/raw-body.middleware.ts
+++ packages/payments-plugin/src/stripe/raw-body.middleware.ts
@@ -1,4 +1,4 @@
-import { raw } from 'body-parser';
+import { json } from 'body-parser';
 import * as http from 'http';
 
 import { RequestWithRawBody } from './types';
@@ -7,7 +7,8 @@ import { RequestWithRawBody } from './types';
  * Middleware which adds the raw request body to the incoming message object. This is needed by
  * Stripe to properly verify webhook events.
  */
-export const rawBodyMiddleware = raw({
+export const rawBodyMiddleware = json({
+    type: '*/*',
     verify(req: RequestWithRawBody, res: http.ServerResponse, buf: Buffer, encoding: string) {
         if (Buffer.isBuffer(buf)) {
             req.rawBody = Buffer.from(buf);
