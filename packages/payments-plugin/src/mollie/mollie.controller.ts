cc4826dfb7c1a2f4e6ed8daa13eb017090d8bd9a
Martijn <martijnvdbrug@users.noreply.github.com>
2023-12-12 08:52:35 +0100

    fix(payments-plugin): Fix Mollie channel awareness (#2575)

--- packages/payments-plugin/src/mollie/mollie.controller.ts
+++ packages/payments-plugin/src/mollie/mollie.controller.ts
@@ -1,17 +1,16 @@
 import { Body, Controller, Param, Post } from '@nestjs/common';
-import { Ctx, Logger, RequestContext, Transaction } from '@vendure/core';
+import { Ctx, Logger, RequestContext, Transaction, ChannelService, LanguageCode } from '@vendure/core';
 
 import { loggerCtx } from './constants';
 import { MollieService } from './mollie.service';
 
 @Controller('payments')
 export class MollieController {
-    constructor(private mollieService: MollieService) {}
+    constructor(private mollieService: MollieService, private channelService: ChannelService) {}
 
     @Post('mollie/:channelToken/:paymentMethodId')
     @Transaction()
     async webhook(
-        @Ctx() ctx: RequestContext,
         @Param('channelToken') channelToken: string,
         @Param('paymentMethodId') paymentMethodId: string,
         @Body() body: any,
@@ -20,8 +19,10 @@ export class MollieController {
             return Logger.warn(' Ignoring incoming webhook, because it has no body.id.', loggerCtx);
         }
         try {
+            // We need to construct a RequestContext based on the channelToken,
+            // because this is an incoming webhook, not a graphql request with a valid Ctx
+            const ctx = await this.createContext(channelToken);
             await this.mollieService.handleMollieStatusUpdate(ctx, {
-                channelToken,
                 paymentMethodId,
                 orderId: body.id,
             });
@@ -33,5 +34,16 @@ export class MollieController {
             );
             throw error;
         }
+    }
+
+    private async createContext(channelToken: string): Promise<RequestContext> {
+        const channel = await this.channelService.getChannelFromToken(channelToken);
+        return new RequestContext({
+            apiType: 'admin',
+            isAuthorized: true,
+            authorizedAsOwnerOnly: false,
+            channel,
+            languageCode: LanguageCode.en,
+        });
     }
 }
