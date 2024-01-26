ba8f60477a31ed4bb4ee0e51e00dee780e34e9bf
Michael Bromley <michael@michaelbromley.co.uk>
2024-01-26 12:41:12 +0100

    test(admin-ui): Add test for localeCurrency pipe with precision 3

--- packages/admin-ui/src/lib/core/src/shared/pipes/locale-currency.pipe.spec.ts
+++ packages/admin-ui/src/lib/core/src/shared/pipes/locale-currency.pipe.spec.ts
@@ -8,7 +8,7 @@ class MockCurrencyService extends CurrencyService {
     constructor(precision = 2) {
         super({
             serverConfig: {
-                moneyStrategyPrecision: 2,
+                moneyStrategyPrecision: precision,
             },
         } as any);
     }
@@ -36,5 +36,12 @@ describe('LocaleCurrencyPipe', () => {
         expect(pipe.transform(1, customCurrencyCode, LanguageCode.en)).toBe('0.01');
         expect(pipe.transform(123, customCurrencyCode, LanguageCode.en)).toBe('1.23');
         expect(pipe.transform(4200000, customCurrencyCode, LanguageCode.en)).toBe('42000.00');
+    });
+
+    it('with precision 3', async () => {
+        const pipe = new LocaleCurrencyPipe(new MockCurrencyService(3));
+        expect(pipe.transform(1, CurrencyCode.GBP, LanguageCode.en)).toBe('£0.001');
+        expect(pipe.transform(123, CurrencyCode.GBP, LanguageCode.en)).toBe('£0.123');
+        expect(pipe.transform(4200000, CurrencyCode.GBP, LanguageCode.en)).toBe('£4,200.000');
     });
 });
