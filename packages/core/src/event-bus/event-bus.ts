1c694996beaaeaec0f78e63c32645c12f219a919
Michael Bromley <michael@michaelbromley.co.uk>
2024-03-19 09:31:45 +0100

    feat(core): Implement new blocking event handler API
    Relates to #2735

--- packages/core/src/event-bus/event-bus.ts
+++ packages/core/src/event-bus/event-bus.ts
@@ -7,9 +7,49 @@ import { EntityManager } from 'typeorm';
 
 import { RequestContext } from '../api/common/request-context';
 import { TRANSACTION_MANAGER_KEY } from '../common/constants';
+import { Logger } from '../config/logger/vendure-logger';
 import { TransactionSubscriber, TransactionSubscriberError } from '../connection/transaction-subscriber';
 
 import { VendureEvent } from './vendure-event';
+
+/**
+ * @description
+ * Options for registering a blocking event handler.
+ *
+ * @since 2.2.0
+ * @docsCategory events
+ */
+export type BlockingEventHandlerOptions<T extends VendureEvent> = {
+    /**
+     * @description
+     * The event type to which the handler should listen.
+     * Can be a single event type or an array of event types.
+     */
+    event: Type<T> | Array<Type<T>>;
+    /**
+     * @description
+     * The handler function which will be executed when the event is published.
+     * If the handler returns a Promise, the event publishing code will wait for the Promise to resolve
+     * before continuing. Any errors thrown by the handler will cause the event publishing code to fail.
+     */
+    handler: (event: T) => void | Promise<void>;
+    /**
+     * @description
+     * A unique identifier for the handler. This can then be used to specify the order in which
+     * handlers should be executed using the `before` and `after` options in other handlers.
+     */
+    id: string;
+    /**
+     * @description
+     * The ID of another handler which this handler should execute before.
+     */
+    before?: string;
+    /**
+     * @description
+     * The ID of another handler which this handler should execute after.
+     */
+    after?: string;
+};
 
 /**
  * @description
@@ -58,6 +98,7 @@ import { VendureEvent } from './vendure-event';
 export class EventBus implements OnModuleDestroy {
     private eventStream = new Subject<VendureEvent>();
     private destroy$ = new Subject<void>();
+    private blockingEventHandlers = new Map<Type<VendureEvent>, Array<BlockingEventHandlerOptions<any>>>();
 
     constructor(private transactionSubscriber: TransactionSubscriber) {}
 
@@ -65,8 +106,9 @@ export class EventBus implements OnModuleDestroy {
      * @description
      * Publish an event which any subscribers can react to.
      */
-    publish<T extends VendureEvent>(event: T): void {
+    async publish<T extends VendureEvent>(event: T): Promise<void> {
         this.eventStream.next(event);
+        await this.executeBlockingEventHandlers(event);
     }
 
     /**
@@ -105,9 +147,134 @@ export class EventBus implements OnModuleDestroy {
         ) as Observable<T>;
     }
 
+    /**
+     * @description
+     * Register an event handler function which will be executed when an event of the given type is published,
+     * and will block execution of the code which published the event until the handler has completed.
+     *
+     * This is useful when you need assurance that the event handler has successfully completed, and you want
+     * the triggering code to fail if the handler fails.
+     *
+     * This API should be used with caution, as errors or performance issues in the handler can cause the
+     * associated operation to be slow or fail entirely. For this reason, any handler which takes longer than
+     * 100ms to execute will log a warning. Any non-trivial task to be performed in a blocking event handler
+     * should be offloaded to a background job using the {@link JobQueueService}.
+     *
+     * @example
+     * ```ts
+     * eventBus.registerBlockingEventHandler({
+     *   event: OrderStateTransitionEvent,
+     *   id: 'my-order-state-transition-handler',
+     *   handler: async (event) => {
+     *     // perform some synchronous task
+     *   }
+     * });
+     * ```
+     *
+     * @since 2.2.0
+     */
+    registerBlockingEventHandler<T extends VendureEvent>(handlerOptions: BlockingEventHandlerOptions<T>) {
+        const events = Array.isArray(handlerOptions.event) ? handlerOptions.event : [handlerOptions.event];
+
+        for (const event of events) {
+            let handlers = this.blockingEventHandlers.get(event);
+            const handlerWithIdAlreadyExists = handlers?.some(h => h.id === handlerOptions.id);
+            if (handlerWithIdAlreadyExists) {
+                throw new Error(
+                    `A handler with the id "${handlerOptions.id}" is already registered for the event ${event.name}`,
+                );
+            }
+
+            if (handlers) {
+                handlers.push(handlerOptions);
+            } else {
+                handlers = [handlerOptions];
+            }
+            const orderedHandlers = this.orderEventHandlers(handlers);
+            this.blockingEventHandlers.set(event, orderedHandlers);
+        }
+    }
+
     /** @internal */
     onModuleDestroy(): any {
         this.destroy$.next();
+    }
+
+    private async executeBlockingEventHandlers<T extends VendureEvent>(event: T): Promise<void> {
+        const blockingHandlers = this.blockingEventHandlers.get(event.constructor as Type<T>);
+        for (const options of blockingHandlers || []) {
+            const timeStart = new Date().getTime();
+            await options.handler(event);
+            const timeEnd = new Date().getTime();
+            const timeTaken = timeEnd - timeStart;
+            Logger.debug(`Blocking event handler ${options.id} took ${timeTaken}ms`);
+            if (timeTaken > 100) {
+                Logger.warn(
+                    [
+                        `Blocking event handler ${options.id} took ${timeTaken}ms`,
+                        `Consider optimizing the handler by moving the logic to a background job or using a more efficient algorithm.`,
+                    ].join('\n'),
+                );
+            }
+        }
+    }
+
+    private orderEventHandlers<T extends VendureEvent>(
+        handlers: Array<BlockingEventHandlerOptions<T>>,
+    ): Array<BlockingEventHandlerOptions<T>> {
+        let orderedHandlers: Array<BlockingEventHandlerOptions<T>> = [];
+        const handlerMap: Map<string, BlockingEventHandlerOptions<T>> = new Map();
+
+        // Create a map of handlers by ID for efficient lookup
+        for (const handler of handlers) {
+            handlerMap.set(handler.id, handler);
+        }
+
+        // Helper function to recursively add handlers in correct order
+        const addHandler = (handler: BlockingEventHandlerOptions<T>) => {
+            // If the handler is already in the ordered list, skip it
+            if (orderedHandlers.includes(handler)) {
+                return;
+            }
+
+            // If an "after" handler is specified, add it recursively
+            if (handler.after) {
+                const afterHandler = handlerMap.get(handler.after);
+                if (afterHandler) {
+                    if (afterHandler.after === handler.id) {
+                        throw new Error(
+                            `Circular dependency detected between event handlers ${handler.id} and ${afterHandler.id}`,
+                        );
+                    }
+                    orderedHandlers = orderedHandlers.filter(h => h.id !== afterHandler.id);
+                    addHandler(afterHandler);
+                }
+            }
+
+            // Add the current handler
+            orderedHandlers.push(handler);
+
+            // If a "before" handler is specified, add it recursively
+            if (handler.before) {
+                const beforeHandler = handlerMap.get(handler.before);
+                if (beforeHandler) {
+                    if (beforeHandler.before === handler.id) {
+                        throw new Error(
+                            `Circular dependency detected between event handlers ${handler.id} and ${beforeHandler.id}`,
+                        );
+                    }
+                    orderedHandlers = orderedHandlers.filter(h => h.id !== beforeHandler.id);
+                    addHandler(beforeHandler);
+                }
+            }
+        };
+
+        // Start adding handlers from the original list
+        for (const handler of handlers) {
+            addHandler(handler);
+        }
+
+        return orderedHandlers;
     }
 
     /**
