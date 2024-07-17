c2e567daf8d767ca98251c0106c92d667e30a508
Michael Bromley <michael@michaelbromley.co.uk>
2024-07-17 13:34:11 +0200

    chore(payments-plugin): Reinstate missing Mollie api types

--- packages/payments-plugin/e2e/graphql/generated-admin-types.ts
+++ packages/payments-plugin/e2e/graphql/generated-admin-types.ts
@@ -1617,6 +1617,7 @@ export enum ErrorCode {
     MANUAL_PAYMENT_STATE_ERROR = 'MANUAL_PAYMENT_STATE_ERROR',
     MIME_TYPE_ERROR = 'MIME_TYPE_ERROR',
     MISSING_CONDITIONS_ERROR = 'MISSING_CONDITIONS_ERROR',
+    MOLLIE_PAYMENT_INTENT_ERROR = 'MOLLIE_PAYMENT_INTENT_ERROR',
     MULTIPLE_ORDER_ERROR = 'MULTIPLE_ORDER_ERROR',
     NATIVE_AUTH_STRATEGY_ERROR = 'NATIVE_AUTH_STRATEGY_ERROR',
     NEGATIVE_QUANTITY_ERROR = 'NEGATIVE_QUANTITY_ERROR',
@@ -1626,6 +1627,7 @@ export enum ErrorCode {
     ORDER_LIMIT_ERROR = 'ORDER_LIMIT_ERROR',
     ORDER_MODIFICATION_ERROR = 'ORDER_MODIFICATION_ERROR',
     ORDER_MODIFICATION_STATE_ERROR = 'ORDER_MODIFICATION_STATE_ERROR',
+    ORDER_PAYMENT_STATE_ERROR = 'ORDER_PAYMENT_STATE_ERROR',
     ORDER_STATE_TRANSITION_ERROR = 'ORDER_STATE_TRANSITION_ERROR',
     PAYMENT_METHOD_MISSING_ERROR = 'PAYMENT_METHOD_MISSING_ERROR',
     PAYMENT_ORDER_MISMATCH_ERROR = 'PAYMENT_ORDER_MISMATCH_ERROR',
@@ -2599,6 +2601,41 @@ export type ModifyOrderResult =
     | PaymentMethodMissingError
     | RefundPaymentIdMissingError;
 
+export type MolliePaymentIntent = {
+    url: Scalars['String']['output'];
+};
+
+export type MolliePaymentIntentError = ErrorResult & {
+    errorCode: ErrorCode;
+    message: Scalars['String']['output'];
+};
+
+export type MolliePaymentIntentInput = {
+    /**
+     * Optional preselected Mollie payment method. When this is passed
+     * the payment selection step will be skipped.
+     */
+    molliePaymentMethodCode?: InputMaybe<Scalars['String']['input']>;
+    /**
+     * Use this to create a payment intent for a specific order. This allows you to create intents for
+     * orders that are not active orders.
+     */
+    orderId?: InputMaybe<Scalars['String']['input']>;
+    /**
+     * The code of the Vendure payment method to use for the payment.
+     * Must have Mollie as payment method handler.
+     * Without this, the first method with Mollie as handler will be used.
+     */
+    paymentMethodCode?: InputMaybe<Scalars['String']['input']>;
+    /**
+     * The redirect url to which the customer will be redirected after the payment is completed.
+     * The configured fallback redirect will be used if this is not provided.
+     */
+    redirectUrl?: InputMaybe<Scalars['String']['input']>;
+};
+
+export type MolliePaymentIntentResult = MolliePaymentIntent | MolliePaymentIntentError;
+
 export type MoveCollectionInput = {
     collectionId: Scalars['ID']['input'];
     index: Scalars['Int']['input'];
@@ -2684,6 +2721,7 @@ export type Mutation = {
     createFacet: Facet;
     /** Create one or more FacetValues */
     createFacetValues: Array<FacetValue>;
+    createMolliePaymentIntent: MolliePaymentIntentResult;
     /** Create existing PaymentMethod */
     createPaymentMethod: PaymentMethod;
     /** Create a new Product */
@@ -2805,12 +2843,7 @@ export type Mutation = {
     duplicateEntity: DuplicateEntityResult;
     flushBufferedJobs: Success;
     importProducts?: Maybe<ImportInfo>;
-    /**
-     * Authenticates the user using the native authentication strategy. This mutation is an alias for authenticate({ native: { ... }})
-     *
-     * The `rememberMe` option applies when using cookie-based sessions, and if `true` it will set the maxAge of the session cookie
-     * to 1 year.
-     */
+    /** Authenticates the user using the native authentication strategy. This mutation is an alias for `authenticate({ native: { ... }})` */
     login: NativeAuthenticationResult;
     logout: Success;
     /**
@@ -3074,6 +3107,10 @@ export type MutationCreateFacetArgs = {
 
 export type MutationCreateFacetValuesArgs = {
     input: Array<CreateFacetValueInput>;
+};
+
+export type MutationCreateMolliePaymentIntentArgs = {
+    input: MolliePaymentIntentInput;
 };
 
 export type MutationCreatePaymentMethodArgs = {
