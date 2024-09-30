5a75177af9d67ed8a489c690025550f7d3a0393d
yazid.amar <hanifi.yazid@gmail.com>
2024-09-30 13:26:07 +0100

    refactor(core,data-import): Allow overriding importer methods

--- packages/core/src/data-import/providers/importer/importer.ts
+++ packages/core/src/data-import/providers/importer/importer.ts
@@ -297,7 +297,7 @@ export class Importer {
         return errors;
     }
 
-    private async getFacetValueIds(
+    protected async getFacetValueIds(
         ctx: RequestContext,
         facets: ParsedFacet[],
         languageCode: LanguageCode,
@@ -363,7 +363,7 @@ export class Importer {
         return facetValueIds;
     }
 
-    private processCustomFieldValues(customFields: { [field: string]: string }, config: CustomFieldConfig[]) {
+    protected processCustomFieldValues(customFields: { [field: string]: string }, config: CustomFieldConfig[]) {
         const processed: { [field: string]: string | string[] | boolean | undefined } = {};
         for (const fieldDef of config) {
             const value = customFields[fieldDef.name];
