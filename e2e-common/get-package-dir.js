dbc8b71e1bcba3190c40f0c0f868933c8428accd
Philipp Sonntag <github@flyp.si>
2024-10-01 14:14:19 +0200

    feat(payments-plugin): Added launch script for e2e tests

--- e2e-common/get-package-dir.js
+++ e2e-common/get-package-dir.js
@@ -4,6 +4,13 @@ const path = require('path');
  * @return {string}
  */
 function getPackageDir() {
+    // Useful for launch scripts that debug specific test files. See the launch.json file in the .vscode folder.
+    const testFile = process.env.TEST_FILE || process.argv.find(arg => arg.startsWith('--package='));
+    if (testFile) {
+        const testFilePath = testFile.split('=')[1] ?? testFile;
+        return path.dirname(testFilePath);
+    }
+
     const packageArg = process.env.PACKAGE || process.argv.find(arg => arg.startsWith('--package='));
     if (!packageArg) {
         console.error('No package specified! Please pass --package=<packageDirName>');
