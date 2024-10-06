755f367d6dcc1307f7d159d45d2c8206ff332700
David Höck <d.hoeck@alpin11.com>
2024-10-06 13:21:32 +0200

    chore(dev-server): Disable BullMQ plugin again

--- packages/dev-server/dev-config.ts
+++ packages/dev-server/dev-config.ts
@@ -78,8 +78,8 @@ export const devConfig: VendureConfig = {
         }),
         DefaultSearchPlugin.init({ bufferUpdates: false, indexStockStatus: false }),
         // Enable if you need to debug the job queue
-        BullMQJobQueuePlugin.init({}),
-        //DefaultJobQueuePlugin.init({}),
+        // BullMQJobQueuePlugin.init({}),
+        DefaultJobQueuePlugin.init({}),
         // JobQueueTestPlugin.init({ queueCount: 10 }),
         // ElasticsearchPlugin.init({
         //     host: 'http://localhost',
