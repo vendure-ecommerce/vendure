d06ab1cd4fd68f2e864eee50919b5a31b6ab7b33
Kevin Mattutat <officialdrayke@outlook.de>
2024-11-19 15:29:32 +0100

    docs: Initial documentation for the implementation of translateable entities

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -92,6 +92,7 @@ const sidebars = {
                     className: 'sidebar-section-header',
                 },
                 'guides/developer-guide/channel-aware/index',
+                'guides/developer-guide/translateable/index',
                 'guides/developer-guide/cache/index',
                 'guides/developer-guide/dataloaders/index',
                 'guides/developer-guide/db-subscribers/index',
