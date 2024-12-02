87a918fe207cec9cc3031ab6276aabf1b271db18
alexis.vigoureux <alexis.vigoureux@swile.co>
2024-12-02 09:22:56 +0100

    fix text

--- packages/core/src/config/vendure-config.ts
+++ packages/core/src/config/vendure-config.ts
@@ -476,7 +476,7 @@ export interface AuthOptions {
     /**
      * @description
      * Disable the last login update (prevent a save on user entity) when a user logs in.
-     * This can be useful when there is a lot of authentication in a short period of time and get lock on the user table.
+     * This can be useful when there are a lot of authentication in a short period of time and there are locks on the user table.
      * @since 3.0.7
      */
     disableLastLoginUpdate?: boolean
