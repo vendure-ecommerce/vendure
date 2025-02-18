66ac2b67ccab061b76334276469ca18947c8baee
Michael Bromley <michael@michaelbromley.co.uk>
2025-02-18 13:28:26 +0100

    chore(dashboard): Set up tailwind v4 + Shadcn ui

--- packages/dashboard/src/routes/index.tsx
+++ packages/dashboard/src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button.js';
 import * as React from 'react';
 import { createFileRoute } from '@tanstack/react-router';
 
@@ -9,6 +10,7 @@ function HomeComponent() {
     return (
         <div className="p-2">
             <h3>Welcome Home!</h3>
+            <Button>Click me</Button>
         </div>
     );
 }
--- packages/dashboard/vite.config.js
+++ packages/dashboard/vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 import react from '@vitejs/plugin-react';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [TanStackRouterVite({ autoCodeSplitting: true }), react()],
+    plugins: [TanStackRouterVite({ autoCodeSplitting: true }), react(), tailwindcss()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
 });
