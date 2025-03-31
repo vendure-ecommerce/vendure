02a98b0ef41312c8193c69f906b5d4988499433d
Michael Bromley <michael@michaelbromley.co.uk>
2025-03-31 15:47:43 +0200

    feat(core): Update to TypeScript v5.8.2

--- packages/create/src/constants.ts
+++ packages/create/src/constants.ts
@@ -4,4 +4,4 @@ export const SERVER_PORT = 3000;
  * The TypeScript version needs to pinned because minor versions often
  * introduce breaking changes.
  */
-export const TYPESCRIPT_VERSION = '5.3.3';
+export const TYPESCRIPT_VERSION = '5.8.2';
