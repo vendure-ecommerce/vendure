22d439369eca6656c44c34808560680cc7cb4e99
Michael Bromley <michael@michaelbromley.co.uk>
2025-03-31 20:44:11 +0200

    chore(admin-ui-plugin): Update app path
    It changed with the Angular update

--- packages/admin-ui-plugin/src/constants.ts
+++ packages/admin-ui-plugin/src/constants.ts
@@ -1,7 +1,7 @@
 import { LanguageCode } from '@vendure/core';
 import path from 'path';
 
-export const DEFAULT_APP_PATH = path.join(__dirname, '../admin-ui');
+export const DEFAULT_APP_PATH = path.join(__dirname, '../admin-ui/browser');
 export const loggerCtx = 'AdminUiPlugin';
 export const defaultLanguage = LanguageCode.en;
 export const defaultLocale = undefined;
