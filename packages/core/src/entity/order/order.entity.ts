068176cee4265416c16a8ffa834b8b7854d12efd
Alexander Berger <47721109+DeltaSAMP@users.noreply.github.com>
2025-06-02 09:41:30 +0200

    feat(core): Throw error on taxSummary if surcharges relation is not loaded (#3569)

--- packages/core/src/entity/order/order.entity.ts
+++ packages/core/src/entity/order/order.entity.ts
@@ -291,9 +291,10 @@ export class Order extends VendureEntity implements ChannelAware, HasCustomField
      * @description
      * A summary of the taxes being applied to this Order.
      */
-    @Calculated({ relations: ['lines'] })
+    @Calculated({ relations: ['lines', 'surcharges'] })
     get taxSummary(): OrderTaxSummary[] {
         this.throwIfLinesNotJoined('taxSummary');
+        this.throwIfSurchargesNotJoined('taxSummary');
         const taxRateMap = new Map<
             string,
             { rate: number; base: number; tax: number; description: string }
@@ -350,6 +351,16 @@ export class Order extends VendureEntity implements ChannelAware, HasCustomField
             const errorMessage = [
                 `The property "${propertyName}" on the Order entity requires the Order.lines relation to be joined.`,
                 "This can be done with the EntityHydratorService: `await entityHydratorService.hydrate(ctx, order, { relations: ['lines'] })`",
+            ];
+
+            throw new InternalServerError(errorMessage.join('\n'));
+        }
+    }
+    private throwIfSurchargesNotJoined(propertyName: keyof Order) {
+        if (this.surcharges == null) {
+            const errorMessage = [
+                `The property "${propertyName}" on the Order entity requires the Order.surcharges relation to be joined.`,
+                "This can be done with the EntityHydratorService: `await entityHydratorService.hydrate(ctx, order, { relations: ['surcharges'] })`",
             ];
 
             throw new InternalServerError(errorMessage.join('\n'));
