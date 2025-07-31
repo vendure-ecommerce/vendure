866afcb42c906136a78c38045e65b630813211e0
David Höck <david@vendure.io>
2025-07-31 12:06:21 +0200

    feat(dashboard): Add customizable login extensions API (#3704)

--- packages/dashboard/src/lib/framework/extension-api/types/index.ts
+++ packages/dashboard/src/lib/framework/extension-api/types/index.ts
@@ -4,5 +4,6 @@ export * from './data-table.js';
 export * from './detail-forms.js';
 export * from './form-components.js';
 export * from './layout.js';
+export * from './login.js';
 export * from './navigation.js';
 export * from './widgets.js';
