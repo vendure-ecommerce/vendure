2a1d18180bc31921545ca7230b5e19dcdce20847
Housein Abo Shaar <76689341+GogoIsProgramming@users.noreply.github.com>
2025-08-04 17:59:24 +0200

    feat(docs): Add copy as md button

---
+++ docs/src/theme/DocItem/Layout/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '@theme-original/DocItem/Layout';
+import CopyMarkdownButton from '@site/src/components/CopyMarkdownButton';
+
+export default function LayoutWrapper(props) {
+  return (
+    <>
+      <Layout {...props} />
+    </>
+  );
+}
