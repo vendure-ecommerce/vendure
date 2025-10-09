1578c1f98befccba5f3ef3b971fa9311ab98d105
Michael Bromley <michael@michaelbromley.co.uk>
2025-10-09 21:43:41 +0200

    feat(cli): Improved monorepo detection and support

--- packages/cli/src/commands/schema/schema.ts
+++ packages/cli/src/commands/schema/schema.ts
@@ -6,7 +6,7 @@ import { withInteractiveTimeout } from '../../utilities/utils';
 const cancelledMessage = 'Schema generation cancelled.';
 
 export interface SchemaOptions {
-    api?: 'admin' | 'shop';
+    api: 'admin' | 'shop';
     format?: 'sdl' | 'json';
     fileName?: string;
     outputDir?: string;
@@ -18,7 +18,7 @@ export interface SchemaOptions {
  */
 export async function schemaCommand(options?: SchemaOptions) {
     // Check if any non-interactive options are provided
-    if (options) {
+    if (options?.api) {
         // Non-interactive mode
         await handleNonInteractiveMode(options);
         return;
