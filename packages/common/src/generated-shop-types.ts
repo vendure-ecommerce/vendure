1f987dc17eab82b0b91cdff02ac1bab3066fdf28
aelhor <aelhor90@gmail.com>
2025-12-08 23:09:17 +0200

    feat(core): Add set order currency code mutation

--- packages/common/src/generated-shop-types.ts
+++ packages/common/src/generated-shop-types.ts
@@ -1273,6 +1273,7 @@ export enum HistoryEntryType {
     ORDER_CANCELLATION = 'ORDER_CANCELLATION',
     ORDER_COUPON_APPLIED = 'ORDER_COUPON_APPLIED',
     ORDER_COUPON_REMOVED = 'ORDER_COUPON_REMOVED',
+    ORDER_CURRENCY_UPDATED = 'ORDER_CURRENCY_UPDATED',
     ORDER_CUSTOMER_UPDATED = 'ORDER_CUSTOMER_UPDATED',
     ORDER_FULFILLMENT = 'ORDER_FULFILLMENT',
     ORDER_FULFILLMENT_TRANSITION = 'ORDER_FULFILLMENT_TRANSITION',
@@ -1822,6 +1823,7 @@ export type Mutation = {
     requestUpdateCustomerEmailAddress: RequestUpdateCustomerEmailAddressResult;
     /** Resets a Customer's password based on the provided token */
     resetPassword: ResetPasswordResult;
+    setCurrencyCodeForOrder: UpdateOrderItemsResult;
     /** Set the Customer for the Order. Required only if the Customer is not currently logged in */
     setCustomerForOrder: SetCustomerForOrderResult;
     /** Sets the billing address for the active Order */
@@ -1932,6 +1934,10 @@ export type MutationRequestUpdateCustomerEmailAddressArgs = {
 export type MutationResetPasswordArgs = {
     password: Scalars['String']['input'];
     token: Scalars['String']['input'];
+};
+
+export type MutationSetCurrencyCodeForOrderArgs = {
+    currencyCode: CurrencyCode;
 };
 
 export type MutationSetCustomerForOrderArgs = {
