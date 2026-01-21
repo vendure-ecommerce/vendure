2b17d760fc7db6bf2fa4ea6eb0e035dc94e5df02
Martijn <martijn.brug@gmail.com>
2026-01-21 11:32:36 +0100

    fix(payments-plugin): Docs

--- packages/payments-plugin/src/mollie/api-extensions.ts
+++ packages/payments-plugin/src/mollie/api-extensions.ts
@@ -28,6 +28,7 @@ const commonSchemaExtensions = gql`
         Set this to false when you expect that order fulfillment takes longer than 24 hours.
         If set to false, you will need to settle the "Authorized" payment in Vendure manually!
         If you fail to do so, the Authorized payment will expire after 28 days.
+        This setting can be overridden on the plugin level via the plugin options.
         """
         immediateCapture: Boolean
         """
