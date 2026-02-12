2db372209f2d8732ad8a0edf045154a26a7c1ff9
ryrahul <caulirahul6@gmail.com>
2026-02-12 14:40:22 +0545

    fix(core): Persist customFields in updateGlobalSettings mutation

--- packages/core/src/service/services/global-settings.service.ts
+++ packages/core/src/service/services/global-settings.service.ts
@@ -77,8 +77,9 @@ export class GlobalSettingsService {
     async updateSettings(ctx: RequestContext, input: UpdateGlobalSettingsInput): Promise<GlobalSettings> {
         const settings = await this.getSettings(ctx);
         await this.eventBus.publish(new GlobalSettingsEvent(ctx, settings, input));
-        patchEntity(settings, input);
         await this.customFieldRelationService.updateRelations(ctx, GlobalSettings, input, settings);
+        patchEntity(settings, input);
+
         return this.connection.getRepository(ctx, GlobalSettings).save(settings);
     }
 }
